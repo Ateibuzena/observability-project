acb2d9b84e87d2e1ff761bb03408e69678530f83
Ateibuzena <ena.ateibuz@gmail.com>
2026-02-22 15:35:01 +0100

    feat: add MetricsInterceptor for tracking HTTP request durations

---
+++ api/src/common/interceptors/metrics.interceptor.ts
@@ -0,0 +1,28 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { MetricsService } from '../../observability/metrics/metrics.service';
+
+@Injectable()
+export class MetricsInterceptor implements NestInterceptor {
+  constructor(private readonly metrics: MetricsService) {}
+
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        const start = Date.now();
+        const request = context.switchToHttp().getRequest();
+        const method = request.method;
+        const route = request.route?.path || request.url;
+
+        return next.handle().pipe(
+            tap(() => {
+                const response = context.switchToHttp().getResponse();
+                const statusCode = response.statusCode;
+                const duration = Date.now() - start;
+
+                this.metrics.httpRequestDuration
+                    .labels(method, route, statusCode.toString())
+                    .observe(duration);
+            }),
+        );
+    }
+}
