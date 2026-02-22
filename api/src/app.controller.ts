0b5fced145780f26c3f8ff10aa4c0414823b9a33
Ateibuzena <ena.ateibuz@gmail.com>
2026-02-22 15:41:57 +0100

    style: format code for consistency and readability

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,18 +1,20 @@
+/*Endpoint /metrics*/
+
 import { Controller, Get, Res } from '@nestjs/common';
 import { MetricsService } from './observability/metrics/metrics.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly metrics: MetricsService) {}
+	constructor(private readonly metrics: MetricsService) { }
 
-  @Get('metrics')
-  async getMetrics(@Res() res) {
-    res.set('Content-Type', 'text/plain');
-    res.send(await this.metrics.getMetrics());
-  }
+	@Get('metrics')
+	async getMetrics(@Res() res) {
+		res.set('Content-Type', 'text/plain');
+		res.send(await this.metrics.getMetrics());
+	}
 
-  @Get('test')
-  test() {
-    return { message: 'ok' };
-  }
+	@Get('test')
+	test() {
+		return { message: 'ok' };
+	}
 }
