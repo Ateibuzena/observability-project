370c2129e3f5f0da37f762b9c78014b42fa7cc97
Ateibuzena <ena.ateibuz@gmail.com>
2026-02-22 15:26:19 +0100

    feat: add MetricsModule and MetricsService for observability metrics

---
+++ api/src/observability/metrics/metrics.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { Registry, collectDefaultMetrics, Histogram } from 'prom-client';
+
+@Injectable()
+export class MetricsService {
+    private readonly registry: Registry;
+    public httpRequestDuration: Histogram<string>;
+
+    constructor() {
+        this.registry = new Registry();
+        collectDefaultMetrics({ register: this.registry });
+
+        this.httpRequestDuration = new Histogram({
+            name: 'http_request_duration_seconds',
+            help: 'Duration of HTTP requests in seconds',
+            labelNames: ['method', 'route', 'status_code'],
+            registers: [this.registry],
+        });
+    }
+
+    getMetrics(): Promise<string> {
+        return this.registry.metrics();
+    }
+}
