db7139db2fc23069d6b118843e9c20630d000913
Ateibuzena <ena.ateibuz@gmail.com>
2026-02-22 15:42:03 +0100

    feat: connect MetricsModule and AppLoggerModule in AppModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,12 @@
+/*Conectar módulos*/
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MetricsModule } from './observability/metrics/metrics.module';
+import { AppLoggerModule } from './observability/logging/logger.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [MetricsModule, AppLoggerModule],
+	controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule { }
